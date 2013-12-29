11a52678c26d6adbb147724ea6c9cc77c2bf76e4
CannibalVox <sdbaynham@gmail.com>
2013-12-29 05:20:35 -0600

    Leather source checkin.

---
+++ ModBootstrapper.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using LeatherLoader.ModList;
+using UnityEngine;
+
+namespace LeatherLoader
+{
+    public class ModBootstrapper : MonoBehaviour
+    {
+        /// <summary>
+        /// List of all currently loaded mods.  Used for not a whole lot currently.
+        /// </summary>
+        List<IModInfo> mLoadedMods = new List<IModInfo>();
+
+        public void Start()
+        {
+            //Set the server to modded if you have Leather installed, in order to be good citizens.
+            //This definintion was obtained by Black Magicks, so please don't be mad, it was for a good cause.
+            Rust.Steam.Server.SetModded();
+
+            //Load the loader info into the list of mods
+            mLoadedMods.Add(new LeatherLoaderInfo());
+
+            ConsoleSystem.Log("Loading mods...");
+
+            string modsFolder = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "mods"));
+
+            if (Directory.Exists(modsFolder))
+            {
+                //If the mods directory exists, cyle through all the assemblies looking for mods
+                //Leather mods have no extension, are in Rust_Server_data or Rust_data and all end in _mod - we do this 
+                //because many server hosts aren't too okay with people uploading random DLLs, so we conceal the DLL-ness 
+                //by removing the extension
+                foreach (string file in Directory.GetFiles(modsFolder, "*_mod"))
+                {
+                    ConsoleSystem.Log(string.Format("Attempting to scan Assembly: {0}", file));
+                    
+                    //Load the assembly & cycle through types
+                    Assembly modFile = Assembly.LoadFile(file);
+
+                    List<Type> behaviours = new List<Type>();
+                    IModInfo modInfo = null;
+                    foreach (Type type in modFile.GetExportedTypes())
+                    {
+                        //I really need to move over to MonoDevelop! Anywhere you see object.ReferenceEquals used it's because the Visual Studio
+                        //version of a class has an equality operator that isn't in Mono
+                        if (!type.IsAbstract && !type.IsGenericType && !object.ReferenceEquals(type.GetConstructor(Type.EmptyTypes), null))
+                        {
+                            //All types we care about are non-generic, non abstract types with a parameterless constructor
+                            if (modInfo == null && typeof(IModInfo).IsAssignableFrom(type))
+                            {
+                                modInfo = (IModInfo)Activator.CreateInstance(type);
+                            } else if (typeof(MonoBehaviour).IsAssignableFrom(type) && type.GetCustomAttributes(typeof(BootstrapAttribute), false).Length != 0)
+                            {
+                                behaviours.Add(type);
+                            }
+                        }
+                    }
+
+                    if (modInfo != null)
+                    {
+                        //We found a modinfo so load the mod & any bootstrap behaviours
+                        ConsoleSystem.Log(string.Format("Located mod '{0}' with {1} MonoBehaviours to bootstrap.", modInfo.GetPrettyModName(), behaviours.Count));
+                        mLoadedMods.Add(modInfo);
+
+                        foreach (Type type in behaviours)
+                        {
+                            //We do one gameobject per script, to allow the bootstrap scripts to do keep-alives on their gameobjects with some level of
+                            //granularity.
+                            new GameObject(type.FullName).AddComponent(type);
+                        }
+                    }
+                }
+            }
+
+            ConsoleSystem.Log("Finished scanning for mods.");
+        }
+    }
+}
---
+++ ModList/IModInfo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeatherLoader.ModList
+{
+    /// <summary>
+    /// I'd like to eventually use this to provide info about what mods are loaded, as well as negotiating requirements between client & server.
+    /// Right now all it does is, all mods need a class with a parameterless constructor that implements this interface in order for us to load
+    /// the scripts.
+    /// </summary>
+    public interface IModInfo
+    {
+        /// <summary>
+        /// Gets the computer-friendly mod name.  We use this mod name to compare whether any two entities have the same mod.
+        /// </summary>
+        string GetModName();
+
+        /// <summary>
+        /// Gets the computer-friendly mod version.  We use this mod name to compare whether any two entities have the same version, so it should only
+        /// increment when compatibility is actually broken.
+        /// </summary>
+        string GetModVersion();
+
+        /// <summary>
+        /// Gets a mod name that's fit to show a user.
+        /// </summary>
+        string GetPrettyModName();
+
+        /// <summary>
+        /// Gets a mod version that's fit to show a user.  Not compared to anything, so feel free to include build numbers or whatever.
+        /// </summary>
+        string GetPrettyModVersion();
+
+        /// <summary>
+        /// Whether a server with this mod should allow clients without the mod to connect.
+        /// </summary>
+        bool CanAcceptModlessClients();
+
+        /// <summary>
+        /// Whether a client with this mod should be able to see & connect to servers without the mod.
+        /// </summary>
+        bool CanConnectToModlessServers();
+
+        /// <summary>
+        /// Get a string that we can show a user which contains the authors responsible.
+        /// </summary>
+        string GetCreditString();
+    }
+}
