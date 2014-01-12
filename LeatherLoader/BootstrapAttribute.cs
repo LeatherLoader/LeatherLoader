4e66ef68b082e02fed860fd2eb755d31d831f50a
CannibalVox <sdbaynham@gmail.com>
2014-01-11 23:01:51 -0600

    Added monodevelop files

---
+++ LeatherLoader/BootstrapAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeatherLoader
+{
+    /// <summary>
+    /// A dummy custom attribute- classes in mods that have this attribute & extend UnityEngine.MonoBehaviour will be loaded as scripts.
+    /// </summary>
+    public class BootstrapAttribute : Attribute
+    {
+    }
+}
---
+++ LeatherLoader/ModList/LeatherLoaderInfo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeatherLoader.ModList
+{
+    /// <summary>
+    /// Version & authoring info for LeatherLoader- ideally will be used some day to check version compatibility, etc.
+    /// </summary>
+    public class LeatherLoaderInfo : IModInfo
+    {
+        public LeatherLoaderInfo()
+        {
+
+        }
+
+        public string GetModName()
+        {
+            return "LEATHERLOADER-LeatherLoader";
+        }
+
+        public string GetModVersion()
+        {
+            return "1.0.0";
+        }
+
+        public string GetPrettyModName()
+        {
+            return "Leather Loader";
+        }
+
+        public string GetPrettyModVersion()
+        {
+            return "Version 1";
+        }
+
+        public bool CanAcceptModlessClients()
+        {
+            return true;
+        }
+
+        public bool CanConnectToModlessServers()
+        {
+            return true;
+        }
+
+        public string GetCreditString()
+        {
+            return "By CanVox";
+        }
+    }
+}
